7bec6fba6a0921cdeb5b197f887dced2ad8b5205
Carly Richmond <carly.richmond@elastic.co>
2026-01-14 14:41:35 +0000

    Clean start

--- components/spinner.tsx
+++
@@ -1,8 +0,0 @@
-/* Plane spinner credit:
-https://codepen.io/ipetak/pen/BaBVaQB
-*/
-export default function Spinner() {
-    return (
-        <div className="spinner"></div>
-    )
-  }
--- src/app/model/fcdo.model.ts
+++
@@ -1,7 +0,0 @@
-export type FCDOResponse = {
-    description: string,
-    base_path: string
-    details: {
-        alert_status: string[]
-    }
-}
